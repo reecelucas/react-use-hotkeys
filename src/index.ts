b853941496f77de6ff33483ea184bbd41e2ff86b
Mikey Stengel <mikey.stengel96@gmail.com>
2020-03-12 23:28:56 +0100

    Check for undefined key events (#9)
    * Fix: Check for event.key being undefined to handle chrome autocomplete
    
    * Chore: Tell husky to use yarn instead of npm and change quotes so that formatting works on Windows
    
    * chore: Early exit when chrome autocomplete triggers keydown event

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,12 @@ const useHotkeys = (
     };
 
     const onKeydown = (event: KeyboardEvent) => {
+      // chrome autocomplete triggers 'keydown' event but event.key will be
+      // undefined. See https://bugs.chromium.org/p/chromium/issues/detail?id=581537
+      if (event.key === undefined) {
+        return;
+      }
+
       if (hotkeysArray.length === 1 && hotkeysArray[0] === ESCAPE_HATCH_KEY) {
         /**
          * Provide escape hatch should the user want to perform
